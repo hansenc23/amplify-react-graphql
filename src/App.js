3dfdc34d91f435794f8fe6e8b02ae7335e6ce6b2
Hansen Christian <hansenc216@gmail.com>
2023-05-04 19:57:47 +1000

    make changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,6 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          <code>Hello from V2</code>
         </p>
         <a
           className="App-link"
